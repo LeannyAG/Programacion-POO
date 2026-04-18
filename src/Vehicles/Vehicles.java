f80e00a34a37c268a3032000e8d41a852facac83
leanny <lalvenog@miumg.edu.gt>
2026-04-17 23:03:31 -0600

    herencia

---
+++ src/Vehicles/Vehicles.java
@@ -0,0 +1,30 @@
+package Vehicles;
+
+public class Vehicles {
+
+	
+	
+	protected String licensePlate;
+    private String owner;
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+}
+	
+	
+	
+
+
